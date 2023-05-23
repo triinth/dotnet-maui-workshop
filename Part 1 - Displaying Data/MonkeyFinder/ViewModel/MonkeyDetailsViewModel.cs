37e055d708b5e2540146164856232cc3e4354a7f
triinth <triintahk@gmail.com>
2023-05-23 11:49:02 +0300

    MonkeyDetailsViewModel

--- Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
+++ Part 1 - Displaying Data/MonkeyFinder/ViewModel/MonkeyDetailsViewModel.cs
@@ -1,13 +1,32 @@
 ﻿namespace MonkeyFinder.ViewModel;
 
-public partial class BaseViewModel : ObservableObject
+[QueryProperty(nameof(Monkey), "Monkey")]
+public partial class MonkeyDetailsViewModel : BaseViewModel
 {
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(IsNotBusy))]
-    bool isBusy;
+    IMap map;
+    public MonkeyDetailsViewModel(IMap map)
+    {
+        this.map = map;
+    }
 
     [ObservableProperty]
-    string title;
+    Monkey monkey;
 
-    public bool IsNotBusy => !IsBusy;
+    [RelayCommand]
+    async Task OpenMap()
+    {
+        try
+        {
+            await map.OpenAsync(Monkey.Latitude, Monkey.Longitude, new MapLaunchOptions
+            {
+                Name = Monkey.Name,
+                NavigationMode = NavigationMode.None
+            });
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Unable to launch maps: {ex.Message}");
+            await Shell.Current.DisplayAlert("Error, no Maps app!", ex.Message, "OK");
+        }
+    }
 }
